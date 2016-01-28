6ce3ddb9b923d949e0b3bf55eb5aca14902edae8
FlavioG0 <flavio.giovannangeli@gmail.com>
2016-01-28 20:47:05 +0100

    Sistemazione versione 1.3
    Rimossa notifica via SMS. Lasciata solo notifica via PushOver.

--- py.viaggia.treno.py
+++ py.viaggia.treno.py
@@ -1,7 +1,7 @@
 # Controllo puntualita' treni
 # ViaggiaTreno Trenitalia
-# v 1.2
-# Autore:flavio.giovannangeli@mail.com
+# v 1.3
+# Autore:flavio.giovannangeli@gmail.com
 
 import sys
 import time
@@ -18,64 +18,73 @@ DEBUG = 1
 POV_POAT = 'aduK5mgyT1LXP7xego3b4MCPDVETxz'
 POV_POUK = 'uTr1vsTiphNFCH1qvi8q7PPTeCpR1X'
 POV_ADDR = 'api.pushover.net:443'
-# SMS serial port parameters
-SMS_SERPORT = '115200'
-SMS_SERSPEED = '/dev/ttyUSB0'
-SMS_NUMS = '393395882183'
 
 
 # B E G I N ...
 
 def get_train_timeline(trainId):
-    while True:
-        # Effettua chiamata all'URL del servizio ViaggiaTreno con il numero di treno passato.
-        url = 'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/scheda?dettaglio=visualizza&numeroTreno=%d&tipoRicerca=numero&lang=IT' %  int(trainId)
-        if DEBUG == 1:
-            print url
-        req = urllib2.Request(url)
-        res = urllib2.urlopen(req)
-        # Lettura dati pagina
-        page = res.read()
-        if DEBUG == 1:
-            print page
-        soup = BeautifulSoup(page)
-        # Estrapola fermate effettuate
-        mydivs = soup.findAll("div", { "class" : "giaeffettuate" })
-        if DEBUG == 1:
-            print mydivs
-        # Estrapola orario previsto ed effettivo sull'ultima fermata effettuata
-        last_stop = max(mydivs)
-        stop_name = td = last_stop.find('h2')
-        stop_name = stop_name.contents[0].strip()
-        if DEBUG == 1:
-            print stop_name
-        stop_times= last_stop.find_all('strong')
-        if DEBUG == 1:
-            print 'Orario previsto:' + stop_times[0].contents[0].strip()
-            print 'Orario effettivo:' + stop_times[1].contents[0].strip()
-        stop_tprev = stop_times[0].contents[0].strip()
-        stop_teffe = stop_times[1].contents[0].strip()
-        today = time.localtime(time.time())
-        ttoday = time.strftime('%Y-%m-%d', today)
-        fmt = '%Y-%m-%d %H:%M'
-        d1 = datetime.strptime(ttoday + ' ' + stop_tprev, fmt)
-        d2 = datetime.strptime(ttoday + ' ' + stop_teffe, fmt)
-        # Estrapola eventuale ritardo
+    # Effettua chiamata all'URL del servizio ViaggiaTreno con il numero di treno passato.
+    url = 'http://mobile.viaggiatreno.it/vt_pax_internet/mobile/scheda?dettaglio=visualizza&numeroTreno=%d&tipoRicerca=numero&lang=IT' %  int(trainId)
+    if DEBUG == 1:
+        print url
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    # Lettura dati pagina
+    page = res.read()
+    if DEBUG == 1:
+        print page
+    soup = BeautifulSoup(page)
+    # Estrapola fermate effettuate
+    mydivs = soup.findAll("div", { "class" : "giaeffettuate" })
+    if DEBUG == 1:
+        print mydivs
+    # Estrapola orario previsto ed effettivo sull'ultima fermata effettuata
+    last_stop = max(mydivs)
+    stop_name = td = last_stop.find('h2')
+    stop_name = stop_name.contents[0].strip()
+    if DEBUG == 1:
+        print stop_name
+    stop_times= last_stop.find_all('strong')
+    if DEBUG == 1:
+        print 'Orario previsto:' + stop_times[0].contents[0].strip()
+        print 'Orario effettivo:' + stop_times[1].contents[0].strip()
+    stop_tprev = stop_times[0].contents[0].strip()
+    stop_teffe = stop_times[1].contents[0].strip()
+    today = time.localtime(time.time())
+    ttoday = time.strftime('%Y-%m-%d', today)
+    fmt = '%Y-%m-%d %H:%M'
+    d1 = datetime.strptime(ttoday + ' ' + stop_tprev, fmt)
+    d2 = datetime.strptime(ttoday + ' ' + stop_teffe, fmt)
+    # Controlla ritardo/anticipo
+    if d2 > d1:
+        # Ritardo
+        train_status = 'rit'
         diff = d2-d1
-        tdelay = diff.seconds/60
-        # Gestisci ritardo treno
-        if tdelay < 5:
-            # Ritardo compreso minore di 5 min., invia messaggio PushOver
-            msg = 'REG.' + str(trainId) + ' ' + train_trasc(trainId) + ' in ritardo inferiore a 5 min. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
-            pushover_service(msg)
-        elif (tdelay >= 5) and (tdelay < 15):
-            # Ritardo compreso tra 5 e 14 min., invia messaggio PushOver
-            msg = 'REG.' + str(trainId) + ' ' + train_trasc(trainId) + ' in ritardo di ' + str(tdelay) + ' min. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
-            pushover_service(msg)
-        else:
-            # Ritardo uguale o superiore a 15 min, invia SMS
-            msg = 'REG.' + str(trainId) + ' ' + train_trasc(trainId) + ' in ritardo di ' + str(tdelay) + ' min. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
-            sms_service(SMS_NUMS,msg)
+    elif d1 > d2:
+        # Anticipo
+        train_status = 'ant'
+        diff = d1-d2
+    else:
+        # In orario
+        train_status = 'ok'
+        diff = d1-d2
+    tdelay = diff.seconds/60
+    # Gestisci ritardo treno
+    if train_status == 'ant':
+        msg = 'REG.' + str(trainId) + ' in anticipo di ' + str(tdelay) + ' minuti. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
+        if DEBUG == 1:
+            print msg
+    elif train_status == 'rit':
+        msg = 'REG.' + str(trainId) + ' in ritardo di ' + str(tdelay) + ' minuti. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
+        if DEBUG == 1:
+            print msg
+    else:
+        msg = 'REG.' + str(trainId) + ' in orario. Ultimo ril. a ' + str(stop_name) + ' alle ore ' + str(stop_teffe)
+        if DEBUG == 1:
+            print msg
+    # Invia PushOver
+    pushover_service(msg)
+
 
 
 def pushover_service(pomsg):
@@ -99,64 +108,6 @@ def pushover_service(pomsg):
             print sys.stderr.write('ERROR: %s\n' % str(err))
     finally:
         return bOK
-
-
-def sms_service(nums,smstext):
-    bOK = True
-    try:
-        serport = SMS_SERPORT
-        serspeed = SMS_SERSPEED
-        gsmobj = gsmdevice(serport,serspeed)
-        numdest = nums.split(';')
-        i = 0
-        while i < len(numdest):
-            if numdest[i]:
-                if not gsmobj.send_sms(numdest[i],smstext) == True:
-                    bOK = False
-                i = i + 1
-            else:
-                break
-    except Exception, err:
-        bOK = False
-        if DEBUG == 1:
-            print sys.stderr.write('ERROR: %s\n' % str(err))
-    finally:
-        return bOK
-
-
-def train_trasc(num_treno):
-    # Trascodifica tratta.
-    if num_treno == '7205':
-        tratta = 'RTE-VEL 05:28'
-    elif num_treno == '7206':
-        tratta = 'VEL-RTE 06:58'
-    elif num_treno == '7208':
-        tratta = 'VEL-RTE 07:26'
-    elif num_treno == '7223':
-        tratta = 'RTE-VEL 13:28'
-    elif num_treno == '7227':
-        tratta = 'RTE-VEL 14:28'
-    elif num_treno == '7229':
-        tratta = 'RTI-VEL 14:55'
-    elif num_treno == '7271':
-        tratta = 'RTE-VEL 15:28'
-    elif num_treno == '7273':
-        tratta = 'RTI-VEL 15:55'
-    elif num_treno == '7275':
-        tratta = 'RTE-VEL 16:28'
-    elif num_treno == '7237':
-        tratta = 'RTI-VEL 16:55'
-    elif num_treno == '7279':
-        tratta = 'RTE-VEL 17:28'
-    elif num_treno == '7241':
-        tratta = 'RTI-VEL 17:55'
-    elif num_treno == '7243':
-        tratta = 'RTE-VEL 18:28'
-    elif num_treno == '7245':
-        tratta = 'RTI-VEL 18:55'
-    else:
-        tratta = 'TRATTA_ND'
-    return tratta
 
 
 if __name__ == '__main__':
